f42135758ec7b03d44d09e0a206b5ba396385c97
erikik <erik8090@gmail.com>
2025-10-11 00:17:22 +0300

    Remove (for now) history

--- technicolorgateway/__init__.py
+++ technicolorgateway/__init__.py
@@ -23,41 +23,51 @@ class TechnicolorGateway:
     def __init__(self, host, port, user, password) -> None:
         self._host = host
         self._port = port
-        self._uri = f'http://{host}:{port}'
+        self._uri = f"http://{host}:{port}"
         self._user = user
         self._password = password
-        self._br = RoboBrowser(history=True, parser="html.parser")
+        self._br = RoboBrowser(history=False, parser="html.parser")
 
     def srp6authenticate(self):
-
         self._br.open(self._uri)
-        token_tag = self._br.find(lambda tag: tag.has_attr('name') and tag['name'] == 'CSRFtoken')
-        token = token_tag['content']
-        _LOGGER.debug('Got CSRF token: %s', token)
+        token_tag = self._br.find(
+            lambda tag: tag.has_attr("name") and tag["name"] == "CSRFtoken"
+        )
+        token = token_tag["content"]
+        _LOGGER.debug("Got CSRF token: %s", token)
 
-        usr = srp.User(self._user, self._password, hash_alg=srp.SHA256, ng_type=srp.NG_2048)
+        usr = srp.User(
+            self._user, self._password, hash_alg=srp.SHA256, ng_type=srp.NG_2048
+        )
         uname, A = usr.start_authentication()
-        _LOGGER.debug('A value %s', binascii.hexlify(A))
+        _LOGGER.debug("A value %s", binascii.hexlify(A))
 
-        self._br.open(f'{self._uri}/authenticate', method='post',
-                      data=urlencode({'CSRFtoken': token,
-                                      'I': uname, 'A': binascii.hexlify(A)}))
+        self._br.open(
+            f"{self._uri}/authenticate",
+            method="post",
+            data=urlencode({"CSRFtoken": token, "I": uname, "A": binascii.hexlify(A)}),
+        )
         _LOGGER.debug("br.response %s", self._br.response)
         j = json.decoder.JSONDecoder().decode(self._br.parsed.decode())
         _LOGGER.debug("Challenge received: %s", j)
 
-        M = usr.process_challenge(binascii.unhexlify(j['s']), binascii.unhexlify(j['B']))
+        M = usr.process_challenge(
+            binascii.unhexlify(j["s"]), binascii.unhexlify(j["B"])
+        )
         _LOGGER.debug("M value %s", binascii.hexlify(M))
-        self._br.open(f'{self._uri}/authenticate', method='post',
-                      data=urlencode({'CSRFtoken': token, 'M': binascii.hexlify(M)}))
+        self._br.open(
+            f"{self._uri}/authenticate",
+            method="post",
+            data=urlencode({"CSRFtoken": token, "M": binascii.hexlify(M)}),
+        )
         _LOGGER.debug("br.response %s", self._br.response)
         j = json.decoder.JSONDecoder().decode(self._br.parsed.decode())
         _LOGGER.debug("Got response %s", j)
 
-        if 'error' in j:
+        if "error" in j:
             raise Exception("Unable to authenticate (check password?), message:", j)
 
-        usr.verify_session(binascii.unhexlify(j['M']))
+        usr.verify_session(binascii.unhexlify(j["M"]))
         if not usr.authenticated():
             raise Exception("Unable to authenticate")
 
@@ -71,8 +81,11 @@ class TechnicolorGateway:
         except Exception as exception:
             _LOGGER.error("Authentication failed. Exception: %s", exception)
             _LOGGER.info("trying to simple authenticate")
-            self._br.open(f'{self._uri}', method='POST',
-                          data={"username": self._user, "password": self._password})
+            self._br.open(
+                f"{self._uri}",
+                method="POST",
+                data={"username": self._user, "password": self._password},
+            )
             _LOGGER.debug("simple: br.response %s", self._br.response)
             if self._br.response.status_code == 200:
                 _LOGGER.info("simple authenticate success")
